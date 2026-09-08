8f4517998974b9ef4e94dad6cdf1da6054914f97
Douglas <douglas_nunes16@hotmail.com>
2026-09-07 22:27:36 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppMinhasCompras
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
